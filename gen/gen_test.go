fd1ceb18651ecee7c9184f5b67925112f90e1396
stackdump <myork@stackdump.com>
2018-12-03 00:21:01 -0600

    WIP

--- gen/gen_test.go
+++ gen/gen_test.go
@@ -3,17 +3,24 @@ package gen_test
 import (
 	"github.com/FactomProject/ptnet-eventstore/gen"
 	"github.com/stackdump/gopetri/statemachine"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
 )
 
 func TestGenerateStateMachineCode(t *testing.T) {
 	p, s := statemachine.LoadPnmlFromFile("../pnml/counter.xml")
-	src := gen.Generate(p, "CounterV1")
+	src := statemachine.Generate(p, "CounterV1")
 	ioutil.WriteFile("counter.go", src.Bytes(), 0600)
 	println(src.String())
+	assert.Nil(t, nil)
 	_ = s
 }
 
-func TestUsingNew(t *testing.T) {
+func TestUsingGeneratedSource(t *testing.T) {
+	m := gen.CounterV1.StateMachine()
+	m.Init()
+	_, err := m.Transform("INC_0", 1)
+	assert.Nil(t, err)
+
 }
